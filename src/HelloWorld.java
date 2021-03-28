b8992d3e66c0ff73ad54233dc705f1fd97d8cbf5
DJabitskiy <DJOlexandr@gmail.com>
2021-03-28 19:24:55 +0300

    new

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld {
+
+	public static void main(String[] args) 	
+	{	
+		System.out.println("Hello World!");
+		int a=5, b=2, c;
+		String d;
+		c=a+b;
+		System.out.println("a = " + a);
+		System.out.println("b= " + b);
+		System.out.println("значит с = " + c);
+		d="sdf"+a*b;
+		System.out.println(c);
+		System.out.println(d);
+		
+	}
+
+}
